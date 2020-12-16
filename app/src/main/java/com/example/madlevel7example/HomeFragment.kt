901a792067d94d655e8544d8d0f3e39f361ab639
Acdaling <acdaling@gmail.com>
2020-12-16 01:03:21 +0100

    Implemented viewmodel in Homefragment and navigation to create a quiz

--- app/src/main/java/com/example/madlevel7example/HomeFragment.kt
+++ app/src/main/java/com/example/madlevel7example/HomeFragment.kt
@@ -1,21 +1,24 @@
 package com.example.madlevel7example
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class HomeFragment : Fragment() {
+    private val viewModel: QuizViewModel by activityViewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -24,8 +27,26 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        val navController = findNavController()
+
+        //always retrieve quiz  when screen is shown
+        viewModel.getQuiz()
+
+        btCreateQuiz.setOnClickListener {
+            navController.navigate(R.id.action_HomeFragment_to_CreateQuizFragment)
         }
+
+        viewModel.quiz.observe(viewLifecycleOwner, Observer {
+            //make button visible and clickable
+            if (it.answer.isNotBlank() || it.answer.isNotBlank()) {
+                btStartQuiz.alpha = 1.0f
+                btStartQuiz.isClickable = true
+
+                btStartQuiz.setOnClickListener {
+                    navController.navigate(R.id.action_HomeFragment_to_QuizFragment)
+                }
+            }
+        })
+
     }
 }
